eb143225c558be4d90f90c5cb63be659d83a7829
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2024-03-16 20:35:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /** @format */
 
 let arr = [
-    { id: 1, name: "john", age: "18", marks: 80 },
-    { id: 2, name: "jack", age: "20", marks: 85 },
-    { id: 3, name: "karen", age: "19", marks: 35 },
+    { id: 1, name: "john", age: "18", profession:"developer" },
+    { id: 2, name: "jack", age: "20", profession:"developer" },
+    { id: 3, name: "karen", age: "19", profession:"admin" },
   ];
  // console.log(arr[0].marks);
   function PrintStudentswithMap() {
     arr.map((item)=>{
-        if(Number(item.marks)>50){
+        if(item.profession>"developer"){
         console.log(item.name+" "+item.marks);
         }
     });
@@ -17,21 +17,21 @@ let arr = [
   
   function PrintStudentsbyForEach() {
     arr.forEach((item)=>{
-        if(Number(item.marks)>50){
-        console.log(item.name+" "+item.marks);
+        if(item.profession=="developer"){
+        console.log(item.name);
         }
     });
     //Write your code here , just console.log
   }
   
   function addData() {
-    arr.push({id:4,name:"susan",age:"20",marks:45});
+    arr.push({id:4,name:"susan",age:"20",profession:"intern"});
 console.log(arr);  
 }
   
   function removeFailedStudent() {
     arr=arr.filter((item)=>{
-        if(Number(item.marks)>=50){
+        if(Number(item.profession)!="admin"){
             return item;
         }
     });
@@ -39,9 +39,9 @@ console.log(arr);
     //Write your code here, just console.log
   }
   let arr1 = [
-    { id: 7, name: "ravi", age: "18", marks: 55 },
-    { id: 8, name: "raj", age: "20", marks: 41 },
-    { id: 9, name: "ram", age: "19", marks: 61 },
+    { id: 7, name: "ravi", age: "18", profession:"director" },
+    { id: 8, name: "raj", age: "20", profession:"clerk" },
+    { id: 9, name: "ram", age: "19", profession:"ceo" },
   ];
   function concatenateArray() {
   arr= arr.concat(arr1);
